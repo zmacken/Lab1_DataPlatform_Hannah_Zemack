00cba3a2a5ec2da7dd7721d39dad88cad3fa1a54
Hannah Zemack <hannah.zemack@gmail.com>
2026-02-20 04:08:45 +0100

    add: flag code

--- main.py
+++ main.py
@@ -22,11 +22,25 @@ def flag_data(df):
     df = df.copy() #function for flagging wrong data
     df['flag'] = '' #empty flag column
 
-    df.loc[df['currency'].isna(), 'flag'] = 'missing currency' #flag missing currency
+    df.loc[df['currency'].isna(), 'flag'] += 'missing currency' #flag missing currency
 
+    df.loc[df['price'] == 0 , 'flag'] += 'price is zero' #flag price is zero
 
+    df.loc[df['price'] >= 10000, 'flag'] += 'price is greater than 10000' #flag high prices
+
+    df.loc[df['id'].isna(), 'flag'] += 'missing id'
+
+    df.loc[df['name'].isna(), 'flag'] += 'missing name'
+
+    df.loc[df['price'] < 0, 'flag'] += 'negative price'
+
+    df.loc[df['created_at'].isna(), 'flag'] += 'missing created_at'
 
     return df
+
+def rejected_data(df):
+    df = df.copy()
+
 
 
 
@@ -38,9 +52,9 @@ raw_df = pd.read_csv('lab 1 - csv.csv', sep= ';')
 # print(raw_df.dtypes)
 
 #use the function to transform data
-clean_df = transform_data(raw_df)
+transformed_df = transform_data(raw_df)
 
-flagged_df = flag_data(clean_df)
+flagged_df = flag_data(transformed_df)
 
 #print(clean_df)
 
@@ -48,7 +62,7 @@ print("=== RAW ===")
 print(raw_df)
 
 print("=== CLEAN ===")
-print(clean_df)
+print(transformed_df)
 
 print("=== FLAGGED ===")
 print(flagged_df)
