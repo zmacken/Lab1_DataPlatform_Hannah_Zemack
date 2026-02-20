61df2a610f6c24e326b0ddedaae09152b5c6d863
Hannah Zemack <hannah.zemack@gmail.com>
2026-02-20 02:15:48 +0100

    add:tranformation code

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
+def transform_data(df): #function for cleaning up raw data
+    for col in df.columns: #remove blankspaces and into lowercase for uniformity
+        if df[col].dtype == 'object':
+            df[col] = df[col].str.strip()
+            df[col] = df[col].str.lower()
+
+    #transform the price column into numeric value. Using coerce to make sure the program doesn't crash
+    df['price'] = pd.to_numeric(df['price'], errors='coerce')
+
+    #tranform the date to a correct date using coerce to not crash the program
+    df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
+
+
+
 # import data
 raw_df = pd.read_csv('lab 1 - csv.csv', sep= ';')
 
