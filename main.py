4df72ed624f3b188829f90dda803b8ffcdb234d8
Hannah Zemack <hannah.zemack@gmail.com>
2026-02-20 02:26:47 +0100

    add: transform

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
-def transform_data(df): #function for cleaning up raw data
+def transform_data(df):#function for cleaning up raw data
+    df = df.copy()
     for col in df.columns: #remove blankspaces and into lowercase for uniformity
         if df[col].dtype == 'object':
             df[col] = df[col].str.strip()
             df[col] = df[col].str.lower()
+
+    #transform so that currency always is upper
+    df["currency"] = df["currency"].str.upper()
 
     #transform the price column into numeric value. Using coerce to make sure the program doesn't crash
     df['price'] = pd.to_numeric(df['price'], errors='coerce')
@@ -12,14 +16,28 @@ def transform_data(df): #function for cleaning up raw data
     #tranform the date to a correct date using coerce to not crash the program
     df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce')
 
+    return df
+
+def flag_data(df): #function for flagging wrong data
+
 
 
 # import data
 raw_df = pd.read_csv('lab 1 - csv.csv', sep= ';')
 
-print(raw_df.head())
-print(raw_df.info())
-print(raw_df.dtypes)
+#print(raw_df)
+# print(raw_df.info())
+# print(raw_df.dtypes)
 
-#transform data
+#use the function to transform data
+clean_df = transform_data(raw_df)
+
+
+#print(clean_df)
+
+print("=== RAW ===")
+print(raw_df)
+
+print("=== CLEAN ===")
+print(clean_df)
 
